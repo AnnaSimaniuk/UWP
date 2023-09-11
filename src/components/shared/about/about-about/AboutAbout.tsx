9bb4a07fcd49cd7e03613244199141f7d9d45823
Gregory <maklakov.grigory@gmail.com>
2023-09-11 09:19:22 +0200

    add title value for Card

--- src/components/shared/about/about-about/AboutAbout.tsx
+++ src/components/shared/about/about-about/AboutAbout.tsx
@@ -22,8 +22,7 @@ export const AboutAbout = component$(() => {
         {/* Здесь можно добавить breadcrumb*/}
         <div class="mb-10 flex flex-wrap items-center justify-between pt-8 lg:mb-8 lg:pt-8 xl:mb-[57px] xl:justify-start xl:pt-[20px]">
           <h1 class="mb-7 text-4xl font-bold uppercase leading-[1.1] lg:mb-0 lg:text-[60px] xl:text-[min(7vw,120px)]">
-            {aboutUsData.heading}
-            {/* {t("about.heading")} */}
+            {t("about_us.title")}
           </h1>
           <div class="about-page__videoshow xl:mx-auto">
             <AboutImage class="h-auto w-[273px] xl:w-[min(31vw,593px)]" />
@@ -31,27 +30,38 @@ export const AboutAbout = component$(() => {
         </div>
         <div class="border-t border-middleGrey xl:flex">
           <h2 class="mb-7 border-middleGrey pt-5 font-bold uppercase leading-[1.4] lg:pt-8 xl:mb-0 xl:w-[21vw] xl:min-w-[21vw] xl:border-r xl:pt-[50px] xl:pr-[min(1%,50px)] xl:text-[clamp(16px,1.3vw,24px)]">
-            {aboutUsData.subtitle}
-            {/* {t("about.subtitle")} */}
+            {t("about_us.description")}
           </h2>
           <div class="xl:pl-[max(5vw,60px)] xl:pt-[50px] 2xl:pl-[min(8vw,153px)]">
-            {aboutUsData.text.map((paragraph, index) => (
-              <p
-                key={index}
-                class="mb-[26px] leading-[1.35] lg:mb-[24px] xl:mb-[48px] xl:text-[min(2.3vw,32px)] xl:leading-[1.4]"
-              >
-                {paragraph}
-                {/* {t("about.paragraph")} */}
-              </p>
-            ))}
+            <p class="mb-[26px] leading-[1.35] lg:mb-[24px] xl:mb-[48px] xl:text-[min(2.3vw,32px)] xl:leading-[1.4]">
+              {t("about_us.p_1")}
+            </p>
+            <p class="mb-[26px] leading-[1.35] lg:mb-[24px] xl:mb-[48px] xl:text-[min(2.3vw,32px)] xl:leading-[1.4]">
+              {t("about_us.p_2")}
+            </p>
+            <p class="mb-[62px] leading-[1.35] lg:mb-[24px] xl:mb-[48px] xl:text-[min(2.3vw,32px)] xl:leading-[1.4]">
+              {t("about_us.p_3")}
+            </p>
             <div class="flex flex-col">
               <ul class="flex flex-row gap-[5vw] lg:w-[47.5%] lg:self-center xl:w-[62%] xl:self-start">
-                <AboutCardValue title="projects" value="300+" />
-                <AboutCardValue title="years in marketing" value="7+" />
+                <AboutCardValue
+                  title={t("about_us.achievements.text_1")}
+                  value={t("about_us.achievements.text_2")}
+                />
+                <AboutCardValue
+                  title={t("about_us.achievements.text_3")}
+                  value={t("about_us.achievements.text_4")}
+                />
               </ul>
               <ul class="flex flex-row gap-[5vw] lg:w-[47.5%] lg:self-end xl:w-[62%]">
-                <AboutCardValue title="employees" value="50+" />
-                <AboutCardValue title="countries" value="4+" />
+                <AboutCardValue
+                  title={t("about_us.achievements.text_5")}
+                  value={t("about_us.achievements.text_6")}
+                />
+                <AboutCardValue
+                  title={t("about_us.achievements.text_7")}
+                  value={t("about_us.achievements.text_8")}
+                />
               </ul>
             </div>
           </div>
