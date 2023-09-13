e4fa2bf396c9ac742b1cf2e0489c9585ff41a38b
Anna Simaniuk <Anna.shchavinska@gmail.com>
2023-09-13 16:48:07 +0300

    change lang config

--- src/components/ui/modal/ModalFeedback.tsx
+++ src/components/ui/modal/ModalFeedback.tsx
@@ -1,17 +1,19 @@
 import { component$ } from "@builder.io/qwik";
 import { FeedbackForm } from "~/components/form/FeedbackForm";
+import { useTranslate } from "qwik-speak";
 
 export const ModalFeedback = component$(() => {
+  const t = useTranslate();
   return (
     <>
       <h2 class="z-10 mt-[20px] mb-[30px] text-4xl font-bold uppercase leading-[43px] text-white lg:mt-[32px] lg:mb-[24px] lg:leading-[39px] xl:text-[36px] xl:leading-[70px] 2xl:text-[64px]">
-        Let’s discuss the project
+        {t("project_form.title")}
       </h2>
       <h3 class="mb-[38px] text-sm font-medium leading-[15px] tracking-[1px] text-middleGrey">
-        Let’s discuss the project
+        {t("project_form.description")}
       </h3>
       <p class="mb-[21px] text-xs font-bold leading-[13px] tracking-[1px] text-middleGrey">
-        I am interested in
+        {t("project_form.categories_title")}
       </p>
       <FeedbackForm services={true} />
     </>
--- src/lib/actions/language.action.ts
+++ src/lib/actions/language.action.ts
@@ -1,7 +1,7 @@
 import { RequestEventLoader } from "@builder.io/qwik-city";
 
 export const getLanguages = async (requestEvent: RequestEventLoader) => {
-  const lang = requestEvent.params.lang !== "sv" ? "en" : "sv";
+  const lang = requestEvent.params.lang !== "en" ? "sv" : "en";
   const res = await fetch(
     `${requestEvent.env.get("URI_API")}/i18n/${lang}/header.json`,
   );
